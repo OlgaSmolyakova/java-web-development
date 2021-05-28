a0aaed330f509588d2a3d24feb75791ee0361d66
OlgaSmolyakova <ollaniel@gmail.com>
2021-05-28 15:25:16 +0300

    Add files via upload

---
+++ tasks-templates/jwd-quicksort/src/main/java/com/training/qsort/util/Sorts.java
@@ -0,0 +1,16 @@
+package com.training.qsort.util;
+
+public final class Sorts {
+	public static int[] quickSort(int[] array) {
+
+		return null;
+	}
+	
+	public static String[] quickSort(String[] array) {
+
+		return null;
+	}
+	
+	
+	private Sorts() {}
+}
